026e68c0e44efc1be3752354cc3b7ac4a49bbdb1
SandeepKr <code@sandeepkr.com>
2024-08-29 01:08:09 +0530

    config added

---
+++ config/app.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+
+/*
+| App Name
+*/ 
+
+'app_name' => env('APP_NAME','nitrovel'),
+
+
+
+
+
+/*
+| App debug mode
+*/ 
+'app_debug' => (bool) env('APP_DEBUG', false),
+
+
+]
+
+?>
